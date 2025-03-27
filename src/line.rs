9109117e4d51282265331265c06997bafa86583a
Balcus <bbalcus04@gmail.com>
2025-03-27 23:05:23 +0200

    implemented support for some graphemes but not all, next will consider adding support for emojis and other graphemes

--- src/line.rs
+++ src/line.rs
@@ -1,21 +1,24 @@
 use std::{cmp, ops::Range};
+use unicode_segmentation::UnicodeSegmentation;
 
 #[derive(Debug)]
 pub struct Line {
-    string: String,
+    string: Vec<String>,
 }
 
 impl Line {
     pub fn from(line_str: &str) -> Self {
         Self {
-            string: String::from(line_str),
+            string: line_str.graphemes(true)
+            .map(String::from)
+            .collect(),
         }
     }
 
     pub fn get(&self, range: Range<usize>) -> String {
         let start = range.start;
         let end = cmp::min(range.end, self.string.len());
-        self.string.get(start..end).unwrap_or_default().to_string()
+        self.string.get(start..end).unwrap_or_default().join("")
     }
 
     pub fn len(&self) -> usize {
